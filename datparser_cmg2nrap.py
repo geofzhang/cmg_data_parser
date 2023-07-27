71e41d2249890959e1e92f5b143d67415bb07840
Fan Zhang <fan.zh.0518@gmail.com>
2023-07-27 11:00:00 -0600

    used actual data

--- datparser_cmg2nrap.py
+++ datparser_cmg2nrap.py
@@ -1,65 +1,65 @@
 import os
+import re
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-def process_chunk(year, header_lines, data_lines, output_file):
-    # Step 3: Create column headers using rows 2 to 9
-    columns = header_lines[1:9]
 
-    # Step 4: Insert the 'year' into column 1 for every row
-    processed_data = []
-    for line in data_lines:
-        processed_data.append(year + '\t' + line)  # Assuming tab-separated values
+def extract_year_from_line(line):
+    # Use regular expression to find the year in the last set of brackets '()'
+    # match = re.search(r'\((\d{4})', line)
+    match = re.search(r'\((.*?)\)', line)
+    if match:
+        return match.group(1)
+    return None
 
+
+def process_chunk(year, data_lines, output_file, write_headers=False):
     # Step 5: Save the processed data into a new .dat file
-    if os.path.exists(output_file):
-        overwrite = messagebox.askyesno("File Exists", f"Do you want to overwrite {output_file}?")
-        if not overwrite:
-            print(f"Skipping processing for {output_file}")
-            return
-
-    with open(output_file, 'w') as outfile:
-        outfile.write('\t'.join(['Year'] + columns) + '\n')  # Write header line
-        outfile.write('\n'.join(processed_data) + '\n')
+    mode = 'a' if not write_headers else 'w'
+    with open(output_file, mode) as outfile:
+        if write_headers:
+            outfile.write("Year\ti_index\tj_index\tk_index\tx_coord m\ty_coord m\tz_coord m\tGas_Saturation\n")  # Write header line
+        for line in data_lines:
+            processed_line = f"{year}\t{line}"  # Append the 'year' to the beginning of the line
+            outfile.write(processed_line + '\n')
 
 
 def process_large_dat_file(input_file, output_file):
-    # Step 1: Read the file in chunks
-    chunk_size = 10000  # Number of lines to read in each chunk
-    header_lines = []
+    # Step 1: Read the file and process each block
     data_lines = []
-    year = None  # Initialize year to None
+    year = None
+    write_headers = True
 
     with open(input_file, 'r') as infile:
         for line_number, line in enumerate(infile):
-            if line_number == 0:
-                year = line.strip()[-11:-7]  # Extract year from the first line
-                continue  # Skip processing row 1
+            if line.startswith("CMG Results"):
+                # If we encounter a new block, process the previous block first
+                if year and data_lines:
+                    # Skip the first two rows (header lines) for subsequent blocks
+                    process_chunk(year, data_lines[8:], output_file, write_headers)
+                    data_lines.clear()
+                    write_headers = False  # No need to write headers for subsequent blocks
 
-            if 1 <= line_number <= 9:
-                header_lines.append(line.strip())
+                # Extract the year from the line
+                year = extract_year_from_line(line)
             else:
                 data_lines.append(line.strip())
 
-            if line_number % chunk_size == 0:
-                # Step 2: Process the current chunk
-                process_chunk(year, header_lines, data_lines, output_file)
-                header_lines.clear()
-                data_lines.clear()
-
-        # Process the last remaining chunk
-        process_chunk(year, header_lines, data_lines, output_file)
+        # Process the last remaining block
+        if year and data_lines:
+            process_chunk(year, data_lines[8:], output_file, write_headers)
 
 
 def process_large_dat_files(input_folder):
     for filename in os.listdir(input_folder):
-        if filename.endswith(".dat"):
+        if filename.endswith(".gslib"):
             input_file = os.path.join(input_folder, filename)
             output_folder = os.path.join(input_folder, "output")  # Create an "output" folder within the input folder
             os.makedirs(output_folder, exist_ok=True)
 
-            output_file = os.path.join(output_folder, filename[:-4] + "_nrap.dat")
+            output_file = os.path.join(output_folder, filename[:-6] + "_nrap.dat")
             process_large_dat_file(input_file, output_file)
+
 
 def select_input_folder():
     input_folder = filedialog.askdirectory(title="Select Input Folder")
