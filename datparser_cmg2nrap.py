e715f40a798c757c8e7443f57de63c98b2c02bc4
Fan Zhang <fan.zh.0518@gmail.com>
2023-07-31 16:01:26 -0600

    fixed separator issue

--- datparser_cmg2nrap.py
+++ datparser_cmg2nrap.py
@@ -2,74 +2,99 @@ import os
 import re
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from tkinter.ttk import Progressbar
 
-
-def extract_year_from_line(line):
-    # Use regular expression to find the year in the last set of brackets '()'
-    # match = re.search(r'\((\d{4})', line)
+def extract_info_within_parentheses(line):
+    # Use regular expression to find the content within the parentheses '()'
     match = re.search(r'\((.*?)\)', line)
     if match:
         return match.group(1)
     return None
 
 
-def process_chunk(year, data_lines, output_file, write_headers=False):
+def process_chunk(info_within_parentheses, property, data_lines, output_file, write_headers=False):
     # Step 5: Save the processed data into a new .dat file
     mode = 'a' if not write_headers else 'w'
     with open(output_file, mode) as outfile:
         if write_headers:
-            outfile.write("Year\ti_index\tj_index\tk_index\tx_coord m\ty_coord m\tz_coord m\tGas_Saturation\n")  # Write header line
+            last_header = property.split('\t')[-1]  # Extract the last header from the data_lines list of the first chunk
+            header_line = f"Year i_index j_index k_index x_coord_m y_coord_m z_coord_m {last_header}\n"
+            outfile.write(header_line)  # Write header line with the last header from the first chunk
         for line in data_lines:
-            processed_line = f"{year}\t{line}"  # Append the 'year' to the beginning of the line
+            processed_line = info_within_parentheses + " " + line.replace('\t', ' ')  # Replace tabs with spaces
             outfile.write(processed_line + '\n')
 
 
-def process_large_dat_file(input_file, output_file):
-    # Step 1: Read the file and process each block
-    data_lines = []
-    year = None
-    write_headers = True
+def process_large_dat_file(input_file, output_file, progress_var):
+    try:
+        # Step 1: Read the file and process each block
+        data_lines = []
+        year = None
+        write_headers = True
 
-    with open(input_file, 'r') as infile:
-        for line_number, line in enumerate(infile):
-            if line.startswith("CMG Results"):
-                # If we encounter a new block, process the previous block first
-                if year and data_lines:
-                    # Skip the first two rows (header lines) for subsequent blocks
-                    process_chunk(year, data_lines[8:], output_file, write_headers)
-                    data_lines.clear()
-                    write_headers = False  # No need to write headers for subsequent blocks
+        with open(input_file, 'r') as infile:
+            total_lines = sum(1 for _ in infile)  # Get the total number of lines in the input file
+            infile.seek(0)  # Reset the file pointer to the beginning
+            processed_lines = 0
 
-                # Extract the year from the line
-                year = extract_year_from_line(line)
-            else:
-                data_lines.append(line.strip())
+            for line_number, line in enumerate(infile):
+                if line.startswith("CMG Results"):
+                    # If we encounter a new block, process the previous block first
+                    if year and data_lines:
+                        # Skip the first two rows (header lines) for subsequent blocks
+                        process_chunk(year, data_lines[7], data_lines[8:], output_file, write_headers)
+                        data_lines.clear()
+                        write_headers = False  # No need to write headers for subsequent blocks
 
-        # Process the last remaining block
-        if year and data_lines:
-            process_chunk(year, data_lines[8:], output_file, write_headers)
+                    # Extract the year from the line
+                    year = extract_info_within_parentheses(line)
+                else:
+                    data_lines.append(line.strip())
+
+                # Update the progress bar
+                processed_lines += 1
+                progress_var.set(int(processed_lines / total_lines * 100))
+                root.update_idletasks()
+
+            # Process the last remaining block
+            if year and data_lines:
+                process_chunk(year, data_lines[7], data_lines[8:], output_file, write_headers)
+
+        messagebox.showinfo("Processing Complete", f"File {input_file} processed successfully!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while processing {input_file}:\n{str(e)}")
 
 
-def process_large_dat_files(input_folder):
-    for filename in os.listdir(input_folder):
-        if filename.endswith(".gslib"):
-            input_file = os.path.join(input_folder, filename)
-            output_folder = os.path.join(input_folder, "output")  # Create an "output" folder within the input folder
+def process_large_dat_files(input_files):
+    for input_file in input_files:
+        if input_file.endswith(".gslib"):
+            input_folder = os.path.dirname(input_file)
+            output_folder = os.path.join(input_folder, "output")  # Set the output folder to 'output' under the input folder
             os.makedirs(output_folder, exist_ok=True)
 
-            output_file = os.path.join(output_folder, filename[:-6] + "_nrap.dat")
-            process_large_dat_file(input_file, output_file)
+            output_file = os.path.join(output_folder, os.path.splitext(os.path.basename(input_file))[0] + "_nrap.txt")
+
+            # Create a progress bar
+            progress_var = tk.DoubleVar()
+            progress_bar = Progressbar(root, variable=progress_var, maximum=100)
+            progress_bar.pack(fill=tk.X, padx=5, pady=5)
+
+            process_large_dat_file(input_file, output_file, progress_var)
+
+            progress_bar.destroy()
 
 
-def select_input_folder():
-    input_folder = filedialog.askdirectory(title="Select Input Folder")
-    if input_folder:
-        process_large_dat_files(input_folder)
-        print("Processing complete. Check the 'output' folder within the input folder.")
+def select_input_files():
+    input_files = filedialog.askopenfilenames(
+        title="Select Input Files",
+        filetypes=[("GSlib Files", "*.gslib")]
+    )
+    if input_files:
+        process_large_dat_files(input_files)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.withdraw()  # Hide the main window
 
-    select_input_folder()
+    select_input_files()
