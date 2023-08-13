8532187cbb948b8f73d72bfd1ba995ebc8623c1c
Fan Zhang <fan.zh.0518@gmail.com>
2023-08-13 16:24:23 -0500

    Updated Parser logic and GUI performance
    1. Parser now gives simplified headers of all inputs.
    2. Parser now gives the correct format of Date in YYYYMMDD for Spotfire usage.
    3. GUI is now refreshing correctly without frozen.

--- cmg_data_parser.py
+++ cmg_data_parser.py
@@ -1,5 +1,6 @@
 import os
 import re
+import threading
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter.ttk import Progressbar
@@ -11,17 +12,45 @@ def extract_info_within_parentheses(line):
         return match.group(1)
     return None
 
+def format_year(year_str):
+    # A mapping of month abbreviations to their respective numbers
+    month_map = {
+        'Jan': '01',
+        'Feb': '02',
+        'Mar': '03',
+        'Apr': '04',
+        'May': '05',
+        'Jun': '06',
+        'Jul': '07',
+        'Aug': '08',
+        'Sep': '09',
+        'Oct': '10',
+        'Nov': '11',
+        'Dec': '12'
+    }
 
-def process_chunk(info_within_parentheses, property, data_lines, output_file, write_headers=False):
+    year, month, day = year_str.split('-')
+    return f"{year}{month_map[month]}{day}"
+
+def format_property(header):
+    if header == 'Gas_Saturation':
+        formatted_header = 'CO2_Sat'
+    elif header == 'Pres_drop_from_time_zero':
+        formatted_header = 'Pres_drop'
+    return formatted_header
+
+def process_chunk(year, property, data_lines, output_file, write_headers=False):
     # Step 5: Save the processed data into a new .dat file
     mode = 'a' if not write_headers else 'w'
     with open(output_file, mode) as outfile:
         if write_headers:
-            last_header = property.split('\t')[-1]  # Extract the last header from the data_lines list of the first chunk
-            header_line = f"Year i_index j_index k_index x_coord_m y_coord_m z_coord_m {last_header}\n"
+            last_header_line = property.split('\t')[-1]  # Extract the last header from the data_lines list of the first chunk
+            last_header = format_property(last_header_line.split()[0])
+            header_line = f"Year i j k x y z {last_header}\n"
             outfile.write(header_line)  # Write header line with the last header from the first chunk
         for line in data_lines:
-            processed_line = info_within_parentheses + " " + line.replace('\t', ' ')  # Replace tabs with spaces
+            formatted_year = format_year(year)  # Reformat year from the CMG output to: YYYYMMDD
+            processed_line = formatted_year + " " + line.replace('\t', ' ')  # Replace tabs with spaces
             outfile.write(processed_line + '\n')
 
 
@@ -31,6 +60,7 @@ def process_large_dat_file(input_file, output_file, progress_var):
         data_lines = []
         year = None
         write_headers = True
+        UPDATE_FREQUENCY = 500000
 
         with open(input_file, 'r') as infile:
             total_lines = sum(1 for _ in infile)  # Get the total number of lines in the input file
@@ -53,13 +83,18 @@ def process_large_dat_file(input_file, output_file, progress_var):
 
                 # Update the progress bar
                 processed_lines += 1
-                progress_var.set(int(processed_lines / total_lines * 100))
-                root.update_idletasks()
+                if processed_lines % UPDATE_FREQUENCY == 0:
+                    progress_var.set(int(processed_lines / total_lines * 100))
+                    root.update_idletasks()
 
             # Process the last remaining block
             if year and data_lines:
                 process_chunk(year, data_lines[7], data_lines[8:], output_file, write_headers)
 
+
+            # Update one last time at the end to ensure it reaches 100%
+            progress_var.set(100)
+            root.update_idletasks()
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred while processing {input_file}:\n{str(e)}")
 
@@ -70,8 +105,8 @@ def process_large_dat_files(input_files):
             input_folder = os.path.dirname(input_file)
             output_folder = os.path.join(input_folder, "output")  # Set the output folder to 'output' under the input folder
             os.makedirs(output_folder, exist_ok=True)
-
-            output_file = os.path.join(output_folder, os.path.splitext(os.path.basename(input_file))[0] + "_nrap.txt")
+            file_name = os.path.splitext(os.path.basename(input_file))[0]
+            output_file = os.path.join(output_folder, file_name + "_nrap.txt")
 
             # Create a progress bar
             progress_var = tk.DoubleVar()
@@ -81,7 +116,10 @@ def process_large_dat_files(input_files):
             process_large_dat_file(input_file, output_file, progress_var)
 
             progress_bar.destroy()
+    root.after(0, show_completion_message())
 
+def show_completion_message():
+    messagebox.showinfo("Processing Complete", f"Files processed successfully!")
 
 def load_input_files():
     input_files = filedialog.askopenfilenames(
@@ -89,8 +127,7 @@ def load_input_files():
         filetypes=[("GSlib Files", "*.gslib")]
     )
     if input_files:
-        process_large_dat_files(input_files)
-        messagebox.showinfo("Processing Complete", f"Files processed successfully!")
+        threading.Thread(target=process_large_dat_files, args=(input_files,)).start()
 
 def select_input_files():
     load_button = tk.Button(root, text="Load .gslib files", command=load_input_files)
